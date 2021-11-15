7eac8702a402942d433aacc9a084f64c89d1c0ec
aleynayilaz <yilazaleyna.56@gmail.com>
2021-11-15 14:16:43 +0300

    changes

--- main.js
+++ main.js
@@ -37,9 +37,9 @@ function calculateApr(month) {
 
 const header = document.getElementById("myDIV");
 const btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
+    let current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
